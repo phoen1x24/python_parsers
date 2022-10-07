f43cdf7e2d263716620354ec615f9351c144facd
Ruzil <you@example.com>
2022-10-07 11:53:02 +0700

    fix bug with cvs format

--- grab_info_parser/main.py
+++ grab_info_parser/main.py
@@ -1,4 +1,6 @@
 import requests
+import csv
+from datetime import datetime
 from bs4 import BeautifulSoup
 from proxy_config import login, password, proxy
 headers = {
@@ -11,14 +13,67 @@ proxies = {
 }
 
 def get_data(url):
+    cur_date = datetime.now().strftime('%m_%d_%Y')
     response = requests.get(url=url, headers=headers, proxies=proxies)
     print(response)
 
     with open(file='index.html', mode='w') as file:
         file.write(response.text)
 
+    with open(file='index.html') as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+    table = soup.find('table', id='ro5xgenergy')
+
+    data_th = table.find('thead').find_all('tr')[-1].find_all('th')
+
+    table_headers = ['Area']
+    for dth in data_th:
+        dth = dth.text.strip()
+        print(dth)
+        table_headers.append(dth)
+
+    with open(file=f'data_{cur_date}.csv', mode='w') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            (
+                table_headers
+            )
+        )
+
+    tbody_trs = table.find('tbody').find_all('tr')
+
+    data = []
+    for tr in tbody_trs:
+
+        area = tr.find('th').text.strip()
+
+        data_by_month = tr.find_all('td')
+        data = [area]
+        for dbm in data_by_month:
+            if dbm.find('a'):
+                area_data = dbm.find('a').get('href')
+            elif dbm.find('span'):
+                area_data = dbm.find('span').text.strip()
+            else:
+                area_data = 'None'
+
+            data.append(area_data)
+
+        with open(file=f'data_{cur_date}.csv', mode='a') as file:
+            writer = csv.writer(file)
+
+            writer.writerow(
+                (
+                    data
+                )
+            )
+    return 'Work done!'
+
 def main():
-    get_data(url='https://www.bls.gov/regions/midwest/data/AverageEnergyPrices_SelectedAreas_Table.htm')
+    print(get_data(url='https://www.bls.gov/regions/midwest/data/AverageEnergyPrices_SelectedAreas_Table.htm'))
 
 
 if __name__ == '__main__':
