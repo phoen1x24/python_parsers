5810b603ddf42c87414506b3a174c83c9acdfc66
Ruzil <you@example.com>
2022-10-08 22:40:59 +0700

    finish

--- medical_tests_site_parser/main.py
+++ medical_tests_site_parser/main.py
@@ -7,12 +7,57 @@ from datetime import datetime
 
 
 def collect_data():
-    proxies = {
-        'https': f'http://{os.getenv("ZiqWD1sB")}:{os.getenv("JNBe2hnX")}@213.209.134.250:62487'
-    }
+    # proxies = {
+    #     'https': f'http://{os.getenv("ZiqWD1sB")}:{os.getenv("JNBe2hnX")}@213.209.134.250:62487'
+    # }
     t_date = datetime.now().strftime('%d_%m_%Y')
+    response = requests.get(url='https://www.lifetime.plus/api/analysis2')
 
+    # with open(f'info_{t_date}.json', 'w') as file:
+    #     json.dump(response.json(), file, indent=4, ensure_ascii=False)
 
+    categories = response.json()['categories']
+    result = []
+
+    for c in categories:
+        c_name = c.get('name').strip()
+        c_items = c.get('items')
+
+        for item in c_items:
+            item_name = item.get('name').strip()
+            item_price = item.get('price')
+            item_desc = item.get('description').strip()
+
+            if 'β' in item_desc:
+                item_desc = item_desc.replace('β', 'B')
+
+            if 'γ' in item_desc:
+                item_desc = item_desc.replace('γ', 'Y')
+
+            item_wt = item.get('days')
+            item_bio = item.get('biomaterial')
+
+            result.append(
+                [c_name, item_name, item_bio, item_desc, item_price, item_wt]
+            )
+
+        with open(f'result_{t_date}.csv', 'a') as file:
+            writer = csv.writer(file)
+
+            writer.writerow(
+                (
+                    'Категория',
+                    'Анализ',
+                    'Биоматериал',
+                    'Описание',
+                    'Стоимость',
+                    'Готовность дней'
+                )
+            )
+
+            writer.writerows(
+                result
+            )
 
 def main():
     collect_data()
